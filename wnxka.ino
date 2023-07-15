25b47c2f995fdfe80f111b6c4df42ca244ad9bf5
sangji <83937801+leesangji5@users.noreply.github.com>
2023-07-15 12:18:22 +0900

    Update wnxka.ino

--- wnxka.ino
+++ wnxka.ino
@@ -1,45 +1,31 @@
 int pin1 = 7;
 int pin2 = 8;
 int pin3 = 9;
-int pin4 = 10;
-int pin5 = 11;
-int pin6 = 12;
 int st = 0;
+int dt = 2000;
 
 void setup() {
   Serial.begin(9600);
   pinMode(pin1, INPUT);
   pinMode(pin2, INPUT);
   pinMode(pin3, INPUT);
-  pinMode(pin4, INPUT);
-  pinMode(pin5, INPUT);
-  pinMode(pin6, INPUT);
   st = millis();
 }
 
 void loop() {
-  if (digitalRead(pin1) == 1 && millis()-st > 1000){
+  if (digitalRead(pin1) == 1 && millis()-st > dt){
     Serial.println(1);
     st = millis();
+    dt = 3000;
   }
-  else if (digitalRead(pin2) == 2 && millis()-st > 1000){
+  else if (digitalRead(pin2) == 1 && millis()-st > dt){
     Serial.println(2);
     st = millis();
+    dt = 9000;
   }
-  else if (digitalRead(pin3) == 3 && millis()-st > 1000){
+  else if (digitalRead(pin3) == 1 && millis()-st > dt){
     Serial.println(3);
     st = millis();
-  }
-  else if (digitalRead(pin4) == 4 && millis()-st > 1000){
-    Serial.println(4);
-    st = millis();
-  }
-  else if (digitalRead(pin5) == 5 && millis()-st > 1000){
-    Serial.println(5);
-    st = millis();
-  }
-  else if (digitalRead(pin6) == 6 && millis()-st > 1000){
-    Serial.println(6);
-    st = millis();
+    dt = 9000;
   }
 }
